b6e9ee6469e8269fc9dab16839ac4429c76605e5
Amit Wadalkar <amit.wadalkar@datamatter.tech>
2026-05-15 13:25:06 +0530

    test_run_workflow

--- problem-statement-1/pages/base_page.py
+++ problem-statement-1/pages/base_page.py
@@ -15,10 +15,6 @@ class BasePage:
     def fill_text(self, locator: str, text: str):
         self.page.locator(locator).fill(text)
     
-
-
-
-    
     def get_text(self, locator: str) -> str:
         return self.page.locator(locator).text_content()
     
